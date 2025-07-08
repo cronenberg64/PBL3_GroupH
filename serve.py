a72ee61111e55c8cc56215dff201d2443c748909
Jonathan Setiawan <jonathanrustam2@gmail.com>
2025-07-09 00:27:00 +0900

    Update serve.py

--- serve.py
+++ serve.py
@@ -142,4 +142,5 @@ def identify_cat():
         return jsonify({'match_found': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
